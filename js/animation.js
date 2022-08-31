2e17d3594dd1fbf38f7825a1030091d1269c5b4d
17ramdani <17rome01@gmail.com>
2022-08-31 07:56:15 -0500

    v2

--- js/animation.js
+++ js/animation.js
@@ -1,7 +1,7 @@
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
-const textArray = ["Ramdani", ];
+const textArray = ["Ramdani","Rome","Dani", ];
 const typingDelay = 200;
 const erasingDelay = 100;
 const newTextDelay = 2000; // Delay between current and next text
--- js/autoTypingAnimation.js
+++ js/autoTypingAnimation.js
@@ -1,7 +1,7 @@
 setTimeout(function() {
     $('.about-info-inner').typed({
       strings: [
-        "<span>Hi, I am Ramdani </span> Since 2020 I started studying as a Web Developer at one of the campuses in Bandung. Now I am interested in becoming a developer of a website-based software. In college I learned many programming languages and frameworks. But what I like in several programming languages are Laravel, React and Bootstrap. 🙂 In addition to the field of informatics and programming, I also like things in the field of graphic design including in the fields of photography, videography, editing, UI/UX, and animators."
+        "<span>Hi, I am Ramdani </span> Since 2020 I started studying as a Web Developer at one of the campuses in Bandung. Now I am interested in becoming a developer of a website-based software. In college I learned many programming languages and frameworks. But what I like in several programming languages are Laravel, React and Bootstrap. 🙂 <br> <br> In addition to the field of informatics and programming, I also like things in the field of graphic design including in the fields of photography, videography, editing, UI/UX, and animators."
       ],
       typeSpeed:5 ,
       contentType: 'html'
